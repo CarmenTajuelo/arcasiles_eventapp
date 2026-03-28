a2c65d9a250b85f587ea040247195796934ccdcd
MariCarmen Tajuelo <mcarmentajuelo@gmail.com>
2026-03-28 14:03:56 +0100

    feat: create event-list.js component

---
+++ src/event-list.js
@@ -0,0 +1,33 @@
+import { LitElement, html, css } from 'lit';
+import events from '../data/events.json';
+
+
+export class EventList extends LitElement {
+    render() {
+        return html`
+        <div class="event-list">
+            ${events.map(event => html`
+                <event-card
+                    title=${event.title}
+                    date=${event.date}
+                    venue=${event.venue}
+                    link=${event.link}
+                    description=${event.description}
+                    image=${event.image}
+                ></event-card>
+            `)}
+        </div>
+    `
+    }
+    static get styles() {
+        return css`
+        :host{
+        display: block;
+        
+        }
+        `
+    }
+}
+
+
+window.customElements.define('event-list', EventList)
